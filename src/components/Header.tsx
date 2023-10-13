e07b44ab36d89a98ac0791fad47abf6f8f2db49e
Mike Dalton <109972253+mikedaltonmtl@users.noreply.github.com>
2023-10-13 13:42:13 -0400

    Rp 8 header (#5)
    * add mobile nav menu
    
    * add footer to mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Button } from './ui/button'
 import { MoveRight } from 'lucide-react'
-import Link from 'next/link'
+import MobileMenu from './Header/MobileMenu'
 
 export default function Header() {
   return (
@@ -9,32 +10,27 @@ export default function Header() {
       <div className="sticky top-0 z-50 flex w-screen border-b border-white bg-white px-9 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
         <div className="flex h-20 w-full items-center justify-between space-x-3">
           <Image src={'/navbar-logo.png'} alt="logo" width={200} height={200} />
-
-          <div className="flex gap-x-3">
-            <div className="flex items-center justify-center gap-x-12">
-              <Link href="/" className="font-bold text-primary drop-shadow-lg">
-                About Us
-              </Link>
-              <Link
-                href="/"
-                className="text-secondary font-bold drop-shadow-lg"
-              >
-                Pricing
-              </Link>
-              <Link
-                href="/"
-                className="text-secondary font-bold drop-shadow-lg"
-              >
-                How it Works
-              </Link>
-            </div>
+          <MobileMenu />
+          <div className="hidden w-full max-w-[35%] justify-between md:flex">
+            <Link href="/" className="text-secondary font-bold drop-shadow-lg">
+              How it Works
+            </Link>
+            <Link
+              href="/about"
+              className="text-secondary font-bold drop-shadow-lg"
+            >
+              Pricing
+            </Link>
+            <Link href="/" className="font-bold text-primary drop-shadow-lg">
+              About Us
+            </Link>
           </div>
-          <div className="flex gap-x-3">
-            <Button variant={'secondary'} className="h-9">
+          <div className="hidden w-full max-w-[25%] justify-evenly gap-x-2 md:flex">
+            <Button variant={'secondary'} className="h-9 whitespace-nowrap">
               Sign In
               <MoveRight className="w-5" />
             </Button>
-            <Button className="h-9">Schedule a Pickup</Button>
+            <Button className="h-9 whitespace-nowrap">Schedule a Pickup</Button>
           </div>
         </div>
       </div>
