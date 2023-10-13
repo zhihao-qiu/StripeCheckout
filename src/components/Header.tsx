a03d0b37948ce5d35cf59f290335ff27b38f3a0c
mike-dalton <michael.j.dalton@gmail.com>
2023-10-12 21:57:26 -0400

    add mobile nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
 import Image from 'next/image'
-import { Button } from './ui/button'
-import { MoveRight } from 'lucide-react'
 import Link from 'next/link'
+import { Button } from './ui/button'
+import {
+  MoveRight,
+  Combine,
+  Calculator,
+  Users2,
+  Unplug,
+  Truck,
+  MenuSquare,
+} from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetClose,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
 export default function Header() {
   return (
@@ -10,31 +25,111 @@ export default function Header() {
         <div className="flex h-20 w-full items-center justify-between space-x-3">
           <Image src={'/navbar-logo.png'} alt="logo" width={200} height={200} />
 
-          <div className="flex gap-x-3">
-            <div className="flex items-center justify-center gap-x-12">
-              <Link href="/" className="font-bold text-primary drop-shadow-lg">
-                About Us
-              </Link>
-              <Link
-                href="/"
-                className="text-secondary font-bold drop-shadow-lg"
+          {/* mobile hamburger menu */}
+          <nav className="w-full px-2 pt-2 md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2.5}
+                  stroke="currentColor"
+                  className="h-10 w-10"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </SheetTrigger>
+              <SheetContent
+                side="left"
+                className="flex w-screen flex-col items-start justify-start pl-12"
               >
-                Pricing
-              </Link>
-              <Link
-                href="/"
-                className="text-secondary font-bold drop-shadow-lg"
-              >
-                How it Works
-              </Link>
-            </div>
+                <SheetTitle className="mb-8 mt-8">
+                  <Image
+                    src={'/navbar-logo.png'}
+                    alt="logo"
+                    width={200}
+                    height={200}
+                  />
+                </SheetTitle>
+                <p className="flex w-[55%] gap-x-2 border-b border-b-grey pb-2">
+                  <MenuSquare className="w-5" />
+                  Menu
+                </p>
+                <SheetClose asChild>
+                  <Link href="/" className="text-secondary hover:text-primary">
+                    <p className="flex gap-x-2">
+                      <Combine className="w-5" />
+                      How it Works
+                    </p>
+                  </Link>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Link href="/" className="text-secondary  hover:text-primary">
+                    <p className="flex gap-x-2">
+                      <Calculator className="w-5" />
+                      Pricing
+                    </p>
+                  </Link>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Link
+                    href="/about"
+                    className="text-secondary hover:text-primary"
+                  >
+                    <p className="flex gap-x-2">
+                      <Users2 className="w-5" />
+                      About Us
+                    </p>
+                  </Link>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Link
+                    href="/sign-in"
+                    className="text-secondary hover:text-primary"
+                  >
+                    <p className="flex gap-x-2">
+                      <Unplug className="w-5" />
+                      Sign In
+                    </p>
+                  </Link>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Link href="/" className="text-secondary hover:text-primary">
+                    <p className="flex gap-x-2">
+                      <Truck className="w-5" />
+                      Schedule Pickup
+                    </p>
+                  </Link>
+                </SheetClose>
+              </SheetContent>
+            </Sheet>
+          </nav>
+
+          <div className="hidden w-full max-w-[35%] justify-between md:flex">
+            <Link href="/" className="text-secondary font-bold drop-shadow-lg">
+              How it Works
+            </Link>
+            <Link
+              href="/about"
+              className="text-secondary font-bold drop-shadow-lg"
+            >
+              Pricing
+            </Link>
+            <Link href="/" className="font-bold text-primary drop-shadow-lg">
+              About Us
+            </Link>
           </div>
-          <div className="flex gap-x-3">
-            <Button variant={'secondary'} className="h-9">
+          <div className="hidden w-full max-w-[25%] justify-evenly gap-x-2 md:flex">
+            <Button variant={'secondary'} className="h-9 whitespace-nowrap">
               Sign In
               <MoveRight className="w-5" />
             </Button>
-            <Button className="h-9">Schedule a Pickup</Button>
+            <Button className="h-9 whitespace-nowrap">Schedule a Pickup</Button>
           </div>
         </div>
       </div>
