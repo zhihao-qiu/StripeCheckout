011c918e1dab62d3ba398f4c913d648d635f7d40
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-23 16:07:36 -0400

    Updated footer social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,12 +30,30 @@ export default function Footer() {
     <div className="w-full space-y-5 bg-brand p-5">
       <p className="text-center text-lg text-white">Check Us Out</p>
       <ul className="flew-row grid grid-cols-3 place-content-center place-items-center content-center items-center justify-center gap-2 gap-x-6 xs:flex">
-        <FooterButtonLink icon={faInstagram} href="/" />
-        <FooterButtonLink icon={faYoutube} href="/" />
-        <FooterButtonLink icon={faXTwitter} href="/" />
-        <FooterButtonLink icon={faLinkedin} href="/" />
-        <FooterButtonLink icon={faTiktok} href="/" />
-        <FooterButtonLink icon={faFacebook} href="/" />
+        <FooterButtonLink
+          icon={faInstagram}
+          href="https://www.instagram.com/returnpal.ca"
+        />
+        <FooterButtonLink
+          icon={faYoutube}
+          href="https://www.youtube.com/@ReturnPal"
+        />
+        <FooterButtonLink
+          icon={faXTwitter}
+          href="https://twitter.com/ReturnPal_ca"
+        />
+        <FooterButtonLink
+          icon={faLinkedin}
+          href="https://www.linkedin.com/company/return-pal/"
+        />
+        <FooterButtonLink
+          icon={faTiktok}
+          href="https://www.tiktok.com/@returnpal"
+        />
+        <FooterButtonLink
+          icon={faFacebook}
+          href="https://www.facebook.com/profile.php?id=61552099060699"
+        />
       </ul>
     </div>
   )
