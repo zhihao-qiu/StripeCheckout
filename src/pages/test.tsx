7c8817bedc98f4a8463db891c7d21fed50e19542
Randy J <120344418+RandyZJin@users.noreply.github.com>
2023-10-27 12:47:50 -0400

    Rp 25 terms and conditions (#33)
    * Initial commit
    
    * add test page to render component
    
    * add component to show a mock terms of use in dialog window
    
    * Add comments for component usage

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import Terms from '@/components/Terms'
+import { getLayout } from '@/layouts/DefaultLayout'
+
+export default function Test() {
+  return (
+    <>
+      <Terms />
+    </>
+  )
+}
+
+Test.getLayout = getLayout
