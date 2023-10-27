b03a13562175a3eec1a72ef46030e199489a6884
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-27 16:33:51 -0400

    Fix merge error: Header schedule pickup link now links to /return

--- src/components/Headers/DesktopHeader.tsx
+++ src/components/Headers/DesktopHeader.tsx
@@ -44,7 +44,7 @@ export default function DesktopHeader() {
       </div>
       <div className="flex space-x-2 lg:space-x-5">
         <SigninModal headerType="desktop" />
-        <Link href="/pick-date">
+        <Link href="/return">
           <Button className="h-9 w-36">Schedule Pickup</Button>
         </Link>
       </div>
