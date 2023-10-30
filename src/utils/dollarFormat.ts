0ffed055d4e635dbccb6f18bb326ed9c96afe975
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-30 12:47:16 -0400

    Move dollarFormat function into lib/utils

--- src/utils/dollarFormat.ts
+++
@@ -1,8 +0,0 @@
-// Reusable function to format a number as a dollar amount
-export default function dollarFormat(amount: number) {
-  const formatted = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'CAD',
-  }).format(amount)
-  return formatted
-}
