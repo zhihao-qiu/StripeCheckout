b25bed565b2edadcbca300c55fafb5b868597936
Irina G <irina.golionko@gmail.com>
2023-11-25 15:21:40 -0500

    add postal code modal and form

---
+++ src/components/PostalCodeModal.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import PostalCodeForm from './PostalCodeVerification/PostalCodeForm'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@/components/ui/button'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
+import { container, item } from '@styles/framer'
+
+// Default headerType is desktop
+function PostalCodeModal({
+  headerType = 'desktop',
+}: {
+  headerType?: 'desktop' | 'mobile'
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {headerType === 'desktop' ? (
+          <Button variant="secondary" className="h-9 w-fit">
+            Check Availabilty
+          </Button>
+        ) : (
+          <Button
+            variant="link"
+            className="text-secondary h-fit p-0 text-base hover:text-primary hover:no-underline"
+          >
+            <p className="flex gap-x-2">
+              <FontAwesomeIcon icon={faRightToBracket} width={'17'} />
+              Check Availabilty
+            </p>
+          </Button>
+        )}
+      </DialogTrigger>
+      <motion.div
+        className="m-0 h-full w-full p-0"
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
+        <DialogContent className="m-0 flex h-5/6 flex-col flex-nowrap items-center justify-start gap-0 bg-paleBlue p-0">
+          <motion.div variants={item} className="mt-4 h-auto w-[50%] sm:my-6">
+            <Image
+              src="/images/returnpal-short-logo.png"
+              alt="Return Pal logo"
+              width={333}
+              height={134}
+            />
+          </motion.div>
+          <p className="flex w-full items-center justify-center pl-2 text-2xl sm:text-3xl">
+            Check Service Area
+          </p>
+          <PostalCodeForm />
+        </DialogContent>
+      </motion.div>
+    </Dialog>
+  )
+}
+
+export default PostalCodeModal
---
+++ src/components/PostalCodeVerification/PostalCodeForm.tsx
@@ -0,0 +1,82 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@components/ui/form'
+import { Input } from '@components/ui/input'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { Button } from '@components/ui/button'
+import NextArrow from '@components/SvgComponents/NextArrow'
+import { motion } from 'framer-motion'
+import { container, item } from '@styles/framer'
+
+const formSchema = z.object({
+  postalCode: z.string().min(6, 'Please enter valid postal code'),
+})
+
+function PostalCodeForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      postalCode: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <motion.div
+        className="m-0 h-full w-full p-0"
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
+        <div className="flex w-full flex-col items-center">
+          <form
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col items-center justify-start"
+          >
+            <FormField
+              control={form.control}
+              name="postalCode"
+              render={({ field }) => (
+                <FormItem className="mt-4 h-16 sm:mt-6 sm:h-20">
+                  <FormControl>
+                    <motion.div variants={item}>
+                      <Input
+                        className="h-10 w-[200px] rounded-xl border-4 border-primary text-lg placeholder:text-grey sm:h-14 sm:w-[275px]"
+                        type="text"
+                        placeholder="L4HL3P"
+                        {...field}
+                      />
+                    </motion.div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <motion.div variants={item}>
+              <Button
+                type="submit"
+                className="mt-2 h-10 w-[150px] rounded-3xl text-lg sm:h-12 sm:w-[150px]"
+              >
+                Submit&nbsp;&nbsp;
+                <NextArrow />
+              </Button>
+            </motion.div>
+          </form>
+        </div>
+      </motion.div>
+    </Form>
+  )
+}
+
+export default PostalCodeForm
