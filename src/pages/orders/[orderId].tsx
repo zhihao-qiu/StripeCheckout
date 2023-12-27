a9e6b19e792c7c19409ff138a3a6709e853b2de1
Bruce Lee <brucehlee@yahoo.ca>
2023-12-27 14:35:25 -0500

    Updated project structure

--- src/pages/orders/[orderId].tsx
+++ src/pages/orders/[orderId].tsx
@@ -1,7 +1,14 @@
+//Should include PUT Method 
+// should do a call to the api to get the info -(when the order was placed , status based you must adjust ) on the individual order
+//getting the order front the rout itself - because this is a dynamic rout.
+//Need to import the componeents into this page.
+// creating individual componeents 
+//For example the button can be used in many locations
+//Remove all Apis
 //src/components/Orders/OrderStatus.tsx
 
 import React, { useState } from 'react'
-import OrderStatusNodes from '@components/Orders/OrderStatus'
+import OrderStatusNodes from '@components/Orders/OrderStatusNodes'
 
 const OrderStatus: React.FC = () => {
   const [orderStatus, setOrderStatus] = useState('Order Placed')
