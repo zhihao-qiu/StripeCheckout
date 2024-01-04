df079c9e1790a27d1d90847dfee648886357bc0e
Ashwini <ashwinihegde28@gmail.com>
2024-01-04 16:39:21 -0500

    Addition: Added Pagination for orders

--- src/pages/api/orders/index.ts
+++ src/pages/api/orders/index.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import client, { connectDB, disconnectDB } from '@/lib/db'
 import { type Order } from '@/components/DashBoard/types'
+
 const dbName = process.env.DATABASE
 
 export default async function handler(
@@ -10,33 +11,43 @@ export default async function handler(
 ) {
   await connectDB()
 
-  if (req.method === 'GET') {
-    // Get all orders
-    try {
-      const database = client.db(dbName)
-      const orders = database.collection<Order>('orders')
-      const allOrders = await orders.find({}).toArray()
-      res.status(200).json(allOrders)
-    } catch (error) {
-      res.status(500).json({ message: 'Error retrieving orders', error })
-    }
-  } else if (req.method === 'POST') {
-    // Create a new order
-    try {
-      const database = client.db(dbName)
-      const orders = database.collection<Order>('orders')
+  const pageSize = 20
+  const page = parseInt(req.query.page as string) || 1
+
+  try {
+    const database = client.db(dbName)
+    const orders = database.collection<Order>('orders')
+
+    if (req.method === 'GET') {
+      const skip = (page - 1) * pageSize
+      const paginatedOrders = await orders
+        .find({})
+        .skip(skip)
+        .limit(pageSize)
+        .toArray()
+
+      const totalOrdersCount = await orders.countDocuments({})
+      const totalPages = Math.ceil(totalOrdersCount / pageSize)
+
+      res.status(200).json({
+        paginatedOrders,
+        currentPage: page,
+        totalPages,
+        totalOrders: totalOrdersCount,
+      })
+    } else if (req.method === 'POST') {
       const newOrder = req.body as Order
       const result = await orders.insertOne(newOrder)
       res
         .status(200)
         .json({ message: 'Order created successfully', id: result.insertedId })
-    } catch (error) {
-      res.status(500).json({ message: 'Error creating order', error })
+    } else {
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
     }
-  } else {
-    res.setHeader('Allow', ['GET', 'POST'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+  } catch (error) {
+    res.status(500).json({ message: 'Error processing request', error })
+  } finally {
+    await disconnectDB()
   }
-
-  await disconnectDB()
 }
