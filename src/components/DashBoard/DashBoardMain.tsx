816b245ea35090d0be50def3b66395c5827d9eff
Ashwini <ashwinihegde28@gmail.com>
2024-01-22 16:33:02 -0500

    Restored DashBoardMain.tsx

--- src/components/DashBoard/DashBoardMain.tsx
+++ src/components/DashBoard/DashBoardMain.tsx
@@ -12,12 +12,6 @@ import { useRouter } from 'next/navigation'
 import { CiCalendar } from 'react-icons/ci'
 import { FaRegCircleUser, FaRegClock } from 'react-icons/fa6'
 import RecentOrders from '@components/Orders/RecentOrders'
-<<<<<<< HEAD
-
-function DashBoardMain() {
-  const [showProfile, setShowProfile] = useState(false)
-
-=======
 function DashBoardMain({
   userInfo,
   setUserInfo,
@@ -25,7 +19,6 @@ function DashBoardMain({
   userInfo: UserInfo
   setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>
 }) {
->>>>>>> 700c38dffde940fdeada921111a9543a56990fe6
   const cardClassnames =
     'border-l-0 border-r-0 border-t-0 border-b-0 border-black flex h-23 h-23 sm:w-1/3 md:w-1/3 lg:w-1/4  select-none flex-row items-center p-1 bg-white mr-8 text-brand '
   const cardTitleClassnames =
@@ -36,22 +29,10 @@ function DashBoardMain({
   const handleRedirect = (path: string) => {
     router.push(path)
   }
-
   return (
     <section className="lg:p-30 flex flex-col justify-center space-y-8 pb-10 pl-20 pr-20 pt-10 lg:space-y-16">
       <div>
         <Reveal>
-<<<<<<< HEAD
-          <h3 className="mb-6 text-subtitle font-bold lg:text-5xl">
-            Your Dashboard
-          </h3>
-        </Reveal>
-
-        <div className="justify-left mb-12 flex">
-          <Card
-            onClick={() => handleRedirect('/return')}
-            className={`${cardClassnames} mr-4`}
-=======
           <h3 className="mb-6 text-subtitle font-bold lg:text-4xl">
             Your Dashboard
           </h3>
@@ -70,16 +51,6 @@ function DashBoardMain({
             <CardHeader className="flex items-center pl-5">
               <Reveal>
                 <div className="flex items-center">
-<<<<<<< HEAD
-                  {' '}
-                  {/* Added a container */}
-                  <div className="mr-4">
-                    {' '}
-                    {/* Container for the icon */}
-                    <Reveal>
-                      <CiCalendar className="fill-secondary h-12 w-12 xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" />
-                      {/* <HandingPackage className="h-12 w-12 fill-primary xxs:h-16 xxs:w-16 xs:h-24 xs:w-24" /> */}
-=======
                   <div className="mr-4">
                     <Reveal>
                       <CiCalendar className="fill-secondary xs:h-19 xs:w-19 h-12 w-12 xxs:h-16 xxs:w-16" />
@@ -94,13 +65,11 @@ function DashBoardMain({
                         Set up a new pickup for your returns.
                       </CardDescription>
                     </Reveal>
-<<<<<<< HEAD
                   </div>
                 </div>
               </Reveal>
             </CardHeader>
           </Card>
-
           <Card
             onClick={() => handleRedirect('/return')}
             className={`${cardClassnames}`}
@@ -114,11 +83,7 @@ function DashBoardMain({
             <CardHeader className="flex items-center pl-5">
               <Reveal>
                 <div className="flex items-center">
-                  {' '}
-                  {/* Added a container */}
                   <div className="mr-4">
-                    {' '}
-                    {/* Container for the icon */}
                     <Reveal>
                       <FaRegClock className="fill-secondary xs:h-19 xs:w-19 h-12 w-12 xxs:h-16 xxs:w-16" />
                     </Reveal>
@@ -137,14 +102,7 @@ function DashBoardMain({
               </Reveal>
             </CardHeader>
           </Card>
-<<<<<<< HEAD
-
           <Card
-            onClick={handleProfileClick}
-=======
-          <Card
-            onClick={() => handleRedirect('/return')}
->>>>>>> 700c38dffde940fdeada921111a9543a56990fe6
             className={`${cardClassnames}`}
             style={{
               borderLeft: '16px solid black',
@@ -163,7 +121,6 @@ function DashBoardMain({
                   </div>
                   <div>
                     <CardTitle className={cardTitleClassnames}>
->>>>>>> 700c38dffde940fdeada921111a9543a56990fe6
                       Manage Account
                     </CardTitle>
                     <Reveal>
@@ -179,11 +136,6 @@ function DashBoardMain({
         </div>
         <RecentOrders />
       </div>
-<<<<<<< HEAD
-
-      {/* {showProfile && <Profile userInfo={userInfo} setUserInfo={setUserInfo} />} */}
-=======
->>>>>>> 700c38dffde940fdeada921111a9543a56990fe6
     </section>
   )
 }
